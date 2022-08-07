eb845f39ef621cd884bcc69d89c12e5adc242a28
Codekacode <karem.quiroz.m@gmail.com>
2022-08-07 10:33:20 -0500

    Add background to home component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,31 @@
-import React from "react";
+import styled from "styled-components";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
 }
 
 export default Home;
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+
+  &:before {
+    background-image: url("/images/home-background.png");
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: -1;
+  }
+`;
