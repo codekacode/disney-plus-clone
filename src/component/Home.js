c3d0a848d25db467b520c22097646aa429dfaf68
Codekacode <karem.quiroz.m@gmail.com>
2022-08-08 22:58:38 -0500

    Add viewers component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
+import Viewers from "./Viewers";
 
 function Home() {
   return (
     <Container>
       <ImageSlider />
+      <Viewers />
     </Container>
   );
 }
@@ -15,7 +17,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
-
+  overflow-x: hidden;
   &:before {
     background-image: url("/images/home-background.png");
     background-position: center center;
